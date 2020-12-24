78a709b78d98f0b25a1ad94901d9057a984abe86
kilometersvi <milesmilosevich@gmail.com>
2020-12-23 18:17:54 -0800

    configured for hosting

--- src/balls2.js
+++ src/balls2.js
@@ -1,6 +1,6 @@
-import * as THREE from '../build/three.module.js'; 
+//import * as THREE from '../build/three.module.js'; 
 //'../build/three.module.js';
-
+//THREE = window.THREE;
 //import Stats from './jsm/libs/stats.module.js';
 
 let container;//, stats;
@@ -30,7 +30,7 @@ let customUniforms;
 function vertexShader() {
     return `
         varying vec3 vUv;
-        
+
         void main() {
             vUv = position;
             vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
@@ -56,7 +56,7 @@ animate();
 
 
 function Ball(posx, posy, radius, randomInitVelocity){
-    
+
     this.x = posx;
     this.y = posy;
     this.vx = 0;
@@ -67,14 +67,14 @@ function Ball(posx, posy, radius, randomInitVelocity){
     }
     this.ax = 0;
     this.ay = -9.8;
-    
-    
+
+
     this.geometry = new THREE.IcosahedronBufferGeometry( radius, 1 );
     const count = this.geometry.attributes.position.count;
     this.geometry.setAttribute( 'color', new THREE.BufferAttribute( new Float32Array( count * 3 ), 3 ) );
-    
+
     const color = new THREE.Color();
-    
+
     /*
     const material = new THREE.MeshPhongMaterial( {
         color: 0xffffff,
@@ -83,7 +83,7 @@ function Ball(posx, posy, radius, randomInitVelocity){
         shininess: 0
     } );
     */
-    
+
     const material = new THREE.ShaderMaterial({
         uniforms: customUniforms,
         transparent: true,
@@ -99,11 +99,11 @@ function Ball(posx, posy, radius, randomInitVelocity){
     this.mesh.position.x = posx;
     this.mesh.position.y = posy;
     this.mesh.rotation.x = - 1.87;
-    
+
     this.mesh.name = Math.floor(Math.random()*999999);
     scene.add( this.mesh );
     balls.push(this);
-    
+
 }
 Ball.prototype.getPhysicsAttributes = function() {
     return [this.x, this.y, this.vx, this.vy, this.ax, this.ay]
@@ -115,7 +115,7 @@ Ball.prototype.dispose = function() {
 
 
 function init() {
-
+    
     container = document.getElementById( 'container' );
 
     camera = new THREE.PerspectiveCamera( 
@@ -132,28 +132,28 @@ function init() {
     const light = new THREE.DirectionalLight( 0xffffff );
     light.position.set( 0, 0, 1 );
     scene.add( light );
-    
+
     customUniforms = {
         colorA : {value: new THREE.Vector4(255,255,255,255)}, //(0.5,0.02,0.45,0)},//{value: new THREE.Vector3(120,10,100)},//new THREE.Color( "rgba(120,10,100,255)")
         colorB : {value: new THREE.Vector4(255,255,255,255)} //(0.8,0.8,0.8,0)}//{value: new THREE.Vector3(210,210,210)}//new THREE.Color( "rgba(210,210,210,255)")
     };
-    
+
     //shadow
     canvas = document.createElement( 'canvas' );
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     const context = canvas.getContext( '2d' );
-    
+
     gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
     gradient.addColorStop( 0.1, 'rgba(210,210,210,1)' );
     gradient.addColorStop( 1, 'rgba(255,255,255,1)' );
-    
+
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
-    
+
     window.addEventListener( 'resize', onWindowResize, false );
 }
 
@@ -173,14 +173,13 @@ function animate() {
 
     requestAnimationFrame( animate );
 
-    
     current ++;
     if (current > next){
         current = 0;
         next = Math.floor((Math.random() * 1/(3*max_rate)) + 1/(4*max_rate));
         var b = new Ball(Math.random()*windowHalfX*2-windowHalfX, windowHalfY*2.5, radius, 50);
     }
-    
+
     var t = t_base;
     var toPop = []
     for (let i = 0; i < balls.length; i++){
@@ -190,7 +189,7 @@ function animate() {
         var vy0 = balls[i].vy;
         var ax = balls[i].ax;
         var ay = balls[i].ay;
-        
+
         if (y0 < b*-1 || y0 > b || x0 < b*-1 || x0 > b){
             toPop.push(i);
             continue;
@@ -200,7 +199,7 @@ function animate() {
         balls[i].x = x0 + vx0*t + 0.5*ax*t*t;
         balls[i].y = y0 + vy0*t + 0.5*ay*t*t;
         balls[i].mesh.position.set(balls[i].x,balls[i].y,0)
-        
+
         const count = balls[i].geometry.attributes.position.count;
         balls[i].geometry.setAttribute( 'color', new THREE.BufferAttribute( new Float32Array( count * 3 ), 3 ) );
         const color = new THREE.Color();
@@ -241,3 +240,4 @@ function render() {
     renderer.render( scene, camera );
 
 }
+
